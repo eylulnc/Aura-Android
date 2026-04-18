fa6bdd82d8153c1cfd9102a760ef7e9c2c36a8ff
Eylül Naz Can <65118253+eylulnc@users.noreply.github.com>
2026-04-18 09:54:25 +0200

    Revert "Feature: History Screen (#4)" (#5)
    This reverts commit a78403e96f859194f366a55d8a6921fc47c9409a.

--- app/src/main/java/com/github/eylulnc/aura/ui/theme/Color.kt
+++ app/src/main/java/com/github/eylulnc/aura/ui/theme/Color.kt
@@ -2,20 +2,10 @@ package com.github.eylulnc.aura.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-// Light
-val LightBackground = Color(0xFFF9F8F5)
-val LightSurface = Color(0xFFFFFFFF)
-val LightSurfaceSubtle = Color(0xFFF0EFEB)
-val LightTextPrimary = Color(0xFF1A1917)
-val LightTextSecondary = Color(0xFF89857E)
-val LightAccent = Color(0xFF7B96E8)
-val LightBorder = Color(0xFFE8E6E1)
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
 
-// Dark
-val DarkBackground = Color(0xFF111110)
-val DarkSurface = Color(0xFF1C1C1A)
-val DarkSurfaceSubtle = Color(0xFF252523)
-val DarkTextPrimary = Color(0xFFEDECEA)
-val DarkTextSecondary = Color(0xFF706C65)
-val DarkAccent = Color(0xFFA8BCF0)
-val DarkBorder = Color(0xFF2E2E2C)
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
