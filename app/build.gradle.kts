6032f76e44a7d7c0a6a70c60cd57504c15a24b1c
Eylül Naz Can <65118253+eylulnc@users.noreply.github.com>
2026-04-18 09:54:01 +0200

    Revert "Feature: History Screen (#4)"
    This reverts commit a78403e96f859194f366a55d8a6921fc47c9409a.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,6 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.navigation.compose)
     implementation(libs.lifecycle.viewmodel.compose)
     implementation(libs.room.runtime)
