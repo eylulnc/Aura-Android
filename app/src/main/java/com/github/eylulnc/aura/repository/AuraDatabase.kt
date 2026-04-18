6283658968b97d778517166013a47c9ff42e72bd
Eylül Naz Can <65118253+eylulnc@users.noreply.github.com>
2026-04-18 10:00:24 +0200

    prevent duplicate entry

--- app/src/main/java/com/github/eylulnc/aura/repository/AuraDatabase.kt
+++ app/src/main/java/com/github/eylulnc/aura/repository/AuraDatabase.kt
@@ -2,13 +2,24 @@ package com.github.eylulnc.aura.repository
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.github.eylulnc.aura.model.MoodEntry
 
 @Database(
     entities = [MoodEntry::class],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
+
 abstract class AuraDatabase : RoomDatabase() {
     abstract fun moodDao(): MoodDao
+
+    companion object {
+        val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(db: SupportSQLiteDatabase) {
+                db.execSQL("CREATE UNIQUE INDEX IF NOT EXISTS index_mood_entries_date ON mood_entries(date)")
+            }
+        }
+    }
 }
