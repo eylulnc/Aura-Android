e0a7bc484fbab2b18b555023d66131dc2852bdee
Eylül Naz Can <65118253+eylulnc@users.noreply.github.com>
2026-04-21 00:22:40 +0200

    increase min sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         applicationId = "com.github.eylulnc.aura"
-        minSdk = 30
+        minSdk = 31
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
